b247a39b6c5235fcbaca181c1ae3cb769778da7b
Арсений Сидоров <aasidorov_6@edu.hse.ru>
2018-12-03 00:48:49 +0500

    test

--- prog6/Program.cs
+++ prog6/Program.cs
@@ -8,8 +8,100 @@ namespace prog6
 {
     class Program
     {
+        static void InputHandler(out double result, string hellostr = "Введите число: ", string errstr = "Ошибка, введите число повторно",
+            double min = double.MinValue, double max = double.MaxValue)
+        {
+            bool ok = false;
+            do
+            {
+                ok = double.TryParse(Console.ReadLine(), out result);
+                if (!ok || result<min || result>max)
+                {
+                    ok = false;
+                    Console.WriteLine(errstr);
+                }
+            } while (!ok);
+        }
+        static void InputHandler(out int result, 
+            int min = int.MinValue, int max = int.MaxValue, string hellostr = "Введите число: ", string errstr = "Ошибка, введите число повторно")
+        {
+            bool ok = false;
+            do
+            {
+                Console.Write(hellostr);
+                ok = int.TryParse(Console.ReadLine(), out result);
+                if (!ok || result < min || result > max)
+                {
+                    ok = false;
+                    Console.WriteLine(errstr);
+                }
+            } while (!ok);
+        }
+
+        static void ArrayMenu(ref double[,] array)
+        {
+            int answer = 4;
+            do
+            {
+                Console.WriteLine("" +
+                "1. Создание двумерного массива\n" +
+                "2. Печать двумерного массива\n" +
+                "3. Удалить первый столбец, в котором есть число, совпадающее с минимальным элементом.\n" +
+                "4. Назад");
+                InputHandler(out answer, 1, 4);
+
+                switch (answer)
+                {
+                    
+                }
+            } while (answer != 4);
+
+        }
+        static void StringMenu(ref string str)
+        {
+            int answer = 4;
+            do
+            {
+                Console.WriteLine("" +
+                "1. Создание строки\n" +
+                "2. Печать строки\n" +
+                "3. Перевернуть все слова в предложении и отсортировать слова по убыванию длин слов.\n" +
+                "4. Назад");
+                InputHandler(out answer, 1, 4);
+
+                switch (answer)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                }
+            } while (answer != 4);
+        }
+
         static void Main(string[] args)
         {
+            double[,] array = new double[0, 0];
+            string str = "";
+            int answer;
+            // Главное меню
+            do
+            {
+                Console.WriteLine("" +
+                "1. Работа с классом Array\n" +
+                "2. Работа с классом string\n" +
+                "3. Выход");
+                InputHandler(out answer, 1, 3);
+                switch (answer)
+                {
+                    case 1:
+                        ArrayMenu(ref array);
+                        break;
+                    case 2:
+                        StringMenu(ref str);
+                        break;
+                }
+            } while (answer != 3);
         }
     }
 }
